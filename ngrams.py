a2d59bbe6419f76b1eb8ac2f7d339610ffcf9cf6
Lucas Connors <lucas.revolutiontech@gmail.com>
2022-11-06 21:48:25 -0800

    feat: print total words of each length

--- ngrams.py
+++ ngrams.py
@@ -28,23 +28,27 @@ def generate_ngrams_for_word(word: str) -> List[str]:
     return ngrams
 
 def generate_positional_ngrams():
+    word_len_frequency = Counter()
     positional_ngrams = defaultdict(Counter)
 
     for word in words.words():
         word_len = len(word)
         if WORD_LENGTH_MIN <= word_len <= WORD_LENGTH_MAX:
+            word_len_frequency[word_len] += 1
             ngrams = generate_ngrams_for_word(word)
             positional_ngrams[word_len].update(ngrams)
 
-    return positional_ngrams
+    return word_len_frequency, positional_ngrams
 
 def runscript():
-    positional_ngrams = generate_positional_ngrams()
+    word_len_frequency, positional_ngrams = generate_positional_ngrams()
 
-    print("Most common ngrams:")
     for word_len in range(WORD_LENGTH_MIN, WORD_LENGTH_MAX + 1):
-        ngram_count = positional_ngrams[word_len]
-        print(f"{word_len}-Letter word:", ngram_count.most_common(NGRAM_MOST_COMMON_NUM))
+        print(f"{word_len}-Letter word\n===")
+        word_count = word_len_frequency[word_len]
+        print("Total words:", word_count)
+        ngram_counter = positional_ngrams[word_len]
+        print("Most common ngrams:", ngram_counter.most_common(NGRAM_MOST_COMMON_NUM), "\n")
 
 if __name__ == "__main__":
     runscript()
