f09261d4f57a8fd09c5f2616bbddfa062ba7b89b
Lucas Connors <lucas.revolutiontech@gmail.com>
2022-11-06 22:45:08 -0800

    feat: create separate column for each letter position

--- ngrams.py
+++ ngrams.py
@@ -10,8 +10,25 @@ WORD_LENGTH_MIN = 4
 WORD_LENGTH_MAX = 6
 NGRAM_LENGTH_MIN = 2
 NGRAM_LETTER_EMPTY = "█"
-NGRAM_LETTER_SEPARATOR = " "
 NGRAM_FREQUENCY_MIN = 30
+
+def make_ordinal(n: int) -> str:
+    """
+    Convert an integer into its ordinal representation::
+
+        make_ordinal(0)   => '0th'
+        make_ordinal(3)   => '3rd'
+        make_ordinal(122) => '122nd'
+        make_ordinal(213) => '213th'
+
+    Source: https://stackoverflow.com/a/50992575/3241924
+    """
+    n = int(n)
+    if 11 <= (n % 100) <= 13:
+        suffix = "th"
+    else:
+        suffix = ["th", "st", "nd", "rd", "th"][min(n % 10, 4)]
+    return str(n) + suffix
 
 def generate_ngrams_for_word(word: str) -> List[str]:
     ngrams = []
@@ -25,7 +42,7 @@ def generate_ngrams_for_word(word: str) -> List[str]:
                     ngram_letters.append(NGRAM_LETTER_EMPTY)
                 else:
                     ngram_letters.append(letter)
-            ngram = NGRAM_LETTER_SEPARATOR.join(ngram_letters).upper()
+            ngram = "".join(ngram_letters).upper()
             ngrams.append(ngram)
 
     return ngrams
@@ -51,15 +68,18 @@ def runscript():
         print(f"Writing {filename}...")
 
         with open(filename, "w") as f:
+            letter_headers = [f"{make_ordinal(i + 1)} Letter" for i in range(word_len)]
+            headers = [*letter_headers, "Frequency"]
             csvwriter = csv.writer(f)
-            csvwriter.writerow(["Ngram", "Frequency"])
+            csvwriter.writerow(headers)
 
             ngram_counter = positional_ngrams[word_len]
             for ngram, frequency in ngram_counter.most_common():
                 if frequency < NGRAM_FREQUENCY_MIN:
                     # We don't care about incredibly rare ngrams
                     break
-                csvwriter.writerow((ngram, frequency))
+                letter_cells = [letter.replace(NGRAM_LETTER_EMPTY, "") for letter in ngram]
+                csvwriter.writerow([*letter_cells, frequency])
 
         word_count = word_len_frequency[word_len]
         print("Total words:", word_count)
