6c5a048df9ac294b7afe618248c24b89a9c4b572
Lucas Connors <lucas.revolutiontech@gmail.com>
2022-11-06 21:11:55 -0800

    refactor: improve readability

--- ngrams.py
+++ ngrams.py
@@ -1,4 +1,5 @@
 from collections import Counter, defaultdict
+from typing import List
 
 from nltk.corpus import words
 
@@ -7,17 +8,30 @@ WORD_LENGTH_MAX = 6
 NGRAM_LENGTH_MIN = 2
 NGRAM_MOST_COMMON_NUM = 10
 
-def runscript():
+def generate_ngrams_for_word(word: str) -> List[str]:
+    ngrams = []
+
+    word_len = len(word)
+    for ngram_start_pos in range(word_len + 1 - NGRAM_LENGTH_MIN):
+        for ngram_length in range(NGRAM_LENGTH_MIN, word_len + 1 - ngram_start_pos):
+            ngrams.append(f"{ngram_start_pos}-{word[ngram_start_pos:ngram_start_pos + ngram_length]}")
+
+    return ngrams
+
+def generate_positional_ngrams():
     positional_ngrams = defaultdict(Counter)
 
-    # Count ngrams by position
     for word in words.words():
         word_len = len(word)
         if WORD_LENGTH_MIN <= word_len <= WORD_LENGTH_MAX:
-            ngrams = (f"{ngram_start_pos}-{word[ngram_start_pos:ngram_start_pos + ngram_length]}" for ngram_start_pos in range(word_len + 1 - NGRAM_LENGTH_MIN) for ngram_length in range(NGRAM_LENGTH_MIN, word_len - ngram_start_pos + 1))
+            ngrams = generate_ngrams_for_word(word)
             positional_ngrams[word_len].update(ngrams)
 
-    # Provide most common ngrams
+    return positional_ngrams
+
+def runscript():
+    positional_ngrams = generate_positional_ngrams()
+
     print("Most common ngrams:")
     for word_len in range(WORD_LENGTH_MIN, WORD_LENGTH_MAX + 1):
         ngram_count = positional_ngrams[word_len]
