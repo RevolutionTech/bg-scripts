151aba0912bdb8e0c86a014bbc6511b7e92d2972
Lucas Connors <lucas.revolutiontech@gmail.com>
2024-05-12 19:47:18 -0700

    feat: add support for cards with unique backs

--- makepdf/main.py
+++ makepdf/main.py
@@ -1,15 +1,18 @@
-from makepdf.sheet import Sheet
+from makepdf.sheet import BackType, Sheet
 
 
 def runscript():
     boards = Sheet("boards", 144, 108, padding=15)
     boards.generate_pdf()
 
-    letters = Sheet("letters", 60, 84, has_back=True)
+    letters = Sheet("letters", 60, 84, back_type=BackType.SHARED)
     letters.generate_pdf()
 
-    words = Sheet("words", 84, 60, has_back=True)
+    words = Sheet("words", 84, 60, back_type=BackType.SHARED)
     words.generate_pdf()
+
+    goals = Sheet("goals", 84, 60, back_type=BackType.UNIQUE)
+    goals.generate_pdf()
 
 
 if __name__ == "__main__":
--- makepdf/sheet.py
+++ makepdf/sheet.py
@@ -1,4 +1,5 @@
 import csv
+import enum
 import os
 from collections import defaultdict
 
@@ -9,12 +10,24 @@ from makepdf.constants import (
 )
 
 
+class BackType(enum.Enum):
+    NONE = "none"
+    SHARED = "shared"
+    UNIQUE = "unique"
+
+
 class Sheet:
-    def __init__(self, image_type: str, image_width: int, image_height: int, padding: int = 0, has_back: bool = False):
+    def __init__(
+            self,
+            image_type: str,
+            image_width: int,
+            image_height: int,
+            padding: int = 0,
+            back_type: BackType = BackType.NONE
+    ):
         self.output_filename = f"{image_type}.pdf"
         self.images_dir = os.path.join(BASE_IMAGES_DIR, image_type)
-        self.has_back = has_back
-        self.back_filename = os.path.join(self.images_dir, f"{BACK_IMAGE_FILENAME}.png")
+        self.back_type = back_type
 
         self.image_width = image_width
         self.image_height = image_height
@@ -46,12 +59,15 @@ class Sheet:
     def _get_images(self):
         for filename in sorted(os.listdir(self.images_dir)):
             name, ext = os.path.splitext(filename)
-            if ext == f".{IMAGE_EXT}" and (not self.has_back or name != BACK_IMAGE_FILENAME):
+            if ext == f".{IMAGE_EXT}" and (self.back_type != BackType.SHARED or name != BACK_IMAGE_FILENAME):
                 yield filename
 
-    def _add_image_to_pdf(self, image_for_page: int, full_filename: str):
+    def _add_image_to_pdf(self, image_for_page: int, full_filename: str, reverse: bool = False):
         row = image_for_page // self.page_num_cols
         column = image_for_page % self.page_num_cols
+        if reverse:
+            # Print images on reverse from right to left
+            column = self.page_num_cols - column - 1
         self.pdf.image(
             full_filename,
             self.hor_margin + column * (self.image_width + self.padding),
@@ -60,10 +76,14 @@ class Sheet:
             self.image_height,
         )
 
-    def _add_back_page(self):
+    def _add_back_page(self, filenames):
         self.pdf.add_page()
-        for i in range(self.images_per_page):
-            self._add_image_to_pdf(i, self.back_filename)
+        for i, filename in enumerate(filenames):
+            if self.back_type == BackType.UNIQUE:
+                back_filename = os.path.join(self.images_dir, BACK_IMAGE_FILENAME, filename)
+            else:
+                back_filename = os.path.join(self.images_dir, f"{BACK_IMAGE_FILENAME}.png")
+            self._add_image_to_pdf(i, back_filename, reverse=True)
 
     def generate_pdf(self):
         quantities = self._get_quantities()
@@ -71,23 +91,36 @@ class Sheet:
         print(f"Generating PDF {self.output_filename}...")
 
         i = 0
+        is_page_end = False
+        filenames_for_back = []
         for filename in self._get_images():
             card_id, _ = os.path.splitext(filename)
             full_filename = os.path.join(self.images_dir, filename)
 
             quantity = quantities[card_id]
             for _ in range(quantity):
-                image_for_page = i % self.images_per_page
-
-                # Handle the start of a new page
-                is_new_page = image_for_page == 0
-                if is_new_page:
-                    if self.has_back:
-                        self._add_back_page()
+                # Handle the start of a page
+                is_page_start = i % self.images_per_page == 0
+                if is_page_start:
                     self.pdf.add_page()
 
+                # Insert a card into the page
+                image_for_page = i % self.images_per_page
                 self._add_image_to_pdf(image_for_page, full_filename)
+                filenames_for_back.append(filename)
+
+                # Handle the end of a page
                 i += 1
+                is_page_end = i % self.images_per_page == 0
+                if is_page_end:
+                    if self.back_type != BackType.NONE:
+                        self._add_back_page(filenames_for_back)
+                    filenames_for_back = []
+
+        # Final page of backs (if necessary)
+        if not is_page_end and self.back_type != BackType.NONE:
+            self._add_back_page(filenames_for_back)
+
         self.pdf.output(os.path.join(SCRATCH_DIR, self.output_filename), "F")
 
         print("PDF generated successfully!")
