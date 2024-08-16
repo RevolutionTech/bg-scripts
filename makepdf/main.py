f9458253e7f2cedc1393869040206fb58f5d8b15
Lucas Connors <lucas.revolutiontech@gmail.com>
2024-08-16 00:07:06 -0700

    present: change board and word size

--- makepdf/main.py
+++ makepdf/main.py
@@ -2,13 +2,13 @@ from makepdf.sheet import Orientation, BackType, Sheet
 
 
 def runscript():
-    boards = Sheet("boards", 130, 150, orientation=Orientation.LANDSCAPE, padding=5, outer_margin=5)
+    boards = Sheet("boards", 139, 156, orientation=Orientation.LANDSCAPE, padding=3, outer_margin=5)
     boards.generate_pdf()
 
     letters = Sheet("letters", 60, 84, back_type=BackType.SHARED)
     letters.generate_pdf()
 
-    words = Sheet("words", 84, 60, back_type=BackType.SHARED)
+    words = Sheet("words", 84, 41, back_type=BackType.SHARED)
     words.generate_pdf()
 
     goals = Sheet("goals", 84, 60, back_type=BackType.SHARED)
