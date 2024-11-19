5023c57a44e71cb41af288294166ddd8c9119b5c
Lucas Connors <lucas.revolutiontech@gmail.com>
2024-11-19 00:22:44 -0800

    fix: improve dictionary using EOWL

--- ngrams/dictionary.py
+++ ngrams/dictionary.py
@@ -1,20 +1,22 @@
 import csv
 import os
+import string
 
 from nltk.corpus import words as nltk_words
 
 from ngrams.constants import SCRATCH_DIR
 
-NGRAMS_TXT = os.path.join(SCRATCH_DIR, "ngrams.txt")
+EOWL_DIR = os.path.join(SCRATCH_DIR, "eowl")
 
 
 def read_ngram_dictionary():
     nltk_dictionary = set(nltk_words.words())
-    ngram_dictionary = set()
+    eowl_dictionary = set()
 
-    with open(NGRAMS_TXT) as f:
-        csvreader = csv.reader(f)
-        for row, in csvreader:
-            ngram_dictionary.add(row)
+    for letter in string.ascii_uppercase:
+        with open(os.path.join(EOWL_DIR, f"{letter} Words.csv")) as f:
+            csvreader = csv.reader(f)
+            for row, in csvreader:
+                eowl_dictionary.add(row)
 
-    return ngram_dictionary & nltk_dictionary
+    return eowl_dictionary & nltk_dictionary
