54c6092004e974f5c1161edee34f1c1448cc41b6
Lucas Connors <lucas.revolutiontech@gmail.com>
2025-03-13 21:31:35 -0700

    feat: calculate similarities between words
    This will hopefully help us determine which words make sense to keep in the game.

--- polysemy/main.py
+++ polysemy/main.py
@@ -5,11 +5,14 @@ from collections import Counter
 
 from nltk.corpus import wordnet as wn
 from nltk.stem import WordNetLemmatizer
+from scipy.spatial.distance import cosine
+from sentence_transformers import SentenceTransformer
 from wordfreq import top_n_list
 
 from polysemy.constants import SCRATCH_DIR, MOST_COMMON_WORDS_REMOVED, MIN_NUM_DEFINITIONS
 
 
+sbert = SentenceTransformer("all-MiniLM-L6-v2")
 wnl = WordNetLemmatizer()
 
 
@@ -40,6 +43,44 @@ def write_csv(words):
         csvwriter.writerows(words)
 
 
+def read_concrete_words():
+    words = []
+
+    filename = os.path.join(SCRATCH_DIR, "concrete-words.csv")
+    with open(filename, "r") as f:
+        csvreader = csv.reader(f)
+        next(csvreader) # skip header
+        for (word,) in csvreader:
+            words.append(word)
+
+    return words
+
+
+def sbert_similarity(word, phrase):
+    """Compute cosine similarity between a word and a phrase using SBERT."""
+    word_vec = sbert.encode(word)
+    phrase_vec = sbert.encode(phrase)
+    return 1 - cosine(word_vec, phrase_vec)
+
+
+def calculate_similarities(polysemous_words):
+    concrete_words = read_concrete_words()
+
+    filename = os.path.join(SCRATCH_DIR, "word-similarities.csv")
+    with open(filename, "w") as f:
+        csvwriter = csv.writer(f)
+        csvwriter.writerow(["Concrete Word", "Polysemous Word", "Similarity"])
+        for word in concrete_words:
+            for polysemous_word, _ in polysemous_words:
+                try:
+                    similarity = sbert_similarity(polysemous_word, word)
+                except KeyError:
+                    print(f"Could not find {word=} or {polysemous_word=}!")
+                    continue
+
+                csvwriter.writerow([word, polysemous_word, similarity])
+
+
 def runscript():
     top_10000 = top_n_list("en", 10000)
     super_common_words = top_n_list("en", MOST_COMMON_WORDS_REMOVED)
@@ -48,6 +89,7 @@ def runscript():
     polysemous_lemmas = filter_polysemous_words(vocab_lemmas)
     sorted_words = sorted(polysemous_lemmas, key=lambda x: x[1], reverse=True)
     write_csv(sorted_words)
+    calculate_similarities(sorted_words)
 
 
 if __name__ == "__main__":
